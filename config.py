32781f26f977c69faec6465d3e9bfc27d979551d
kirill <kirichka27@gmail.com>
2022-11-15 01:45:01 +0300

    Init commit

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS"))
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = bool(os.getenv("DEBUG"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') \
+                              or "postgresql://postgres:qwerty@localhost:5432/second_test"
+
+
+class TestingConfig(Config):
+    TESTING = bool(os.getenv("TESTING"))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///production.db'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
